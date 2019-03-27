22f011eced7d28ac2d90b4850992c77524738ad2
Tal <c-tal.wanish@charter.com>
2019-03-27 12:14:55 -0600

    Adding Brewery per Capita data and calculations

--- lib/_init_.py
+++ lib/_init_.py
@@ -35,27 +35,29 @@ withHousing = joinData(withIncome, housing)
 pathHousing = os.path.normpath(str(os.getcwd()).split('lib')[0]+'/data/popData.csv')
 population = pd.read_csv(pathHousing, engine='python').rename(columns = {'GEO.id2' : 'ZCTA'})
 
-withPopulation = joinData(withHousing, population)
-
+withPopulation = joinData(withHousing, population).fillna(0)
 ### Determine frequency of brewery counts per ZCTA/State
-
-zipTotals = withPopulation[['ZCTA', 'brewery size']].groupby('ZCTA').count()
-zipByCount = joinData(zipCW, zipTotals)[['zip code', 'STATE', 'ZCTA', 'brewery size']].fillna(0).sort_values('STATE')
-
+zipTotals = withPopulation[['ZCTA', 'popDrinking', 'brewery size']].groupby('ZCTA').agg({'brewery size':'count',
+                          'popDrinking': 'mean'}).rename(index=str, columns={'brewery size': 'brewery count'})
+zipTotals['BpC'] = zipTotals['brewery count']/zipTotals['popDrinking']
+zipTotals=zipTotals.replace(np.inf, 0).reset_index()
+zipTotals['ZCTA'] = zipTotals.astype({'ZCTA': 'float'}).astype({'ZCTA':'int'})
+zipByCount = joinData(zipCW, zipTotals)[['zip code', 'STATE', 'ZCTA', 'brewery count', 'popDrinking', 'BpC']].fillna(0).sort_values('STATE')
 sns.set_style("darkgrid")
-f, statePlots = plt.subplots(8, 7,figsize=(48,27))
-for i, state in enumerate(zipByCount.STATE.unique()):
-    stateData = zipByCount.loc[zipByCount['STATE']==state]
-    stateCount = stateData['brewery size'].values
-    sns.distplot(stateCount, kde=False, bins=range(0,10), norm_hist=True, ax=statePlots[int(i/7), i%7]).set(xlim=(0,10),ylim=(0,1))
-    statePlots[int(i/7), i%7].set_title(state)
-    
-countHist = zipByCount['brewery size'].values
-f.savefig("foo.png")
 
-plt.figure()
-sns.distplot(countHist, kde=False, bins=range(0,10), norm_hist=True).set(xlim=(0,10),ylim=(0,1))
-plt.title("United States")
+#f, statePlots = plt.subplots(8, 7,figsize=(48,27))
+#for i, state in enumerate(zipByCount.STATE.unique()):
+#    stateData = zipByCount.loc[zipByCount['STATE']==state]
+#    stateCount = stateData['brewery count'].values
+#    sns.distplot(stateCount, kde=False, bins=range(0,10), norm_hist=True, ax=statePlots[int(i/7), i%7]).set(xlim=(0,10),ylim=(0,1))
+#    statePlots[int(i/7), i%7].set_title(state)
+#    
+#countHist = zipByCount['brewery count'].values
+#f.savefig("foo.png")
+#
+#plt.figure()
+#sns.distplot(countHist, kde=False, bins=range(0,10), norm_hist=True).set(xlim=(0,10),ylim=(0,1))
+#plt.title("United States")
 
 
 
